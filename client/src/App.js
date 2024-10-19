dcb06f7fe4336ca0a7422da12978df8be5795e62
Deejayayay <122236585+Deejayayay@users.noreply.github.com>
2024-10-18 22:20:18 -0700

    more changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,40 @@
 import React, { useState, useEffect } from 'react'
-
+import './globalstyles.css'
 function App() {
-  const [people, setPeople] = useState([]);
+  const [data, setdata] = useState({
+    name: "",
+    age: 0,
+    date: "",
+    programming: "",
+});
   //gets the data from the json in the database 
   useEffect(() => {
     // Fetch data from Flask API
-    fetch('http://localhost:5000/people')
-      .then(response => response.json())
-      .then(data => setPeople(data))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("/data").then((res) =>
+      res.json().then((data) => {
+          // Setting a data from api
+          setdata({
+              name: data.Name,
+              age: data.Age,
+              date: data.Date,
+              programming: data.programming,
+          });
+      })
+  );
   }, []);
 
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">boto</h1>
       <div className='report-box'>alloted time pomodoroed</div>
       <div className='music-player'>music</div>
       <div className='timer-box'>25:00</div>
 
       <div className='task-box'>      
-      <ul>
-        {people.map((person) => (
-          <li key={person.ssn}>
-            {person.firstName} {person.lastName} - {person.age} years old
-          </li>
-        ))}
-      </ul>
+                <p>{data.name}</p>
+                <p>{data.age}</p>
+                <p>{data.date}</p>
+                <p>{data.programming}</p>
       </div>
 
     </div>
--- flask-server/server.py
+++ flask-server/server.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database import Person, Base
+import datetime
+
+x = datetime.datetime.now()
 
 app = Flask(__name__)
 
@@ -11,15 +14,14 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 #api route to add a new person
-@app.route("/people", methods=["POST"])
+@app.route("/data")
 def get_task():
-
     return {
-        "project": ["p1", "p2", "p3"],
-        "task": ["t1","t2","t3"],
-        "completed": [False, True, True],
-        "note": ["n1", "n2", "n3"]
-    }
+        'Name':"geek", 
+        "Age":"22",
+        "Date":x, 
+        "programming":"python"
+        }
 
 if __name__ == "__main__":
     app.run(debug=True)
